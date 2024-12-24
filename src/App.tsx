a8ebbb6818b1597d07c7f0344fd13e8af33f6dcf
julienrousseau <julrousseau20@gmail.com>
2024-12-24 11:34:26 -0500

    selector + styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
-import { useEffect, useState } from "react";
-import { Link, useLocation, useParams } from "react-router";
+import clsx, { ClassValue } from "clsx";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { NavLink, useLocation, useParams } from "react-router";
+import { twMerge } from "tailwind-merge";
 import recipes from "./recipes";
+
+const MAX = 5;
+const MIN = 1;
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
 
 function App() {
   const params = useParams<{ recipe: string }>();
@@ -11,16 +20,8 @@ function App() {
   return (
     <main className="flex gap-4">
       <Sidebar recipes={recipes} />
-      <div>
-        <input
-          type="range"
-          min="1"
-          max="10"
-          step="1"
-          className="w-24"
-          value={scale}
-          onChange={(e) => setScale(parseInt(e.currentTarget.value))}
-        />
+      <div className="flex flex-grow flex-col">
+        <ScaleSelector scale={scale} setScale={setScale} />
         {recipe && <Recipe {...recipe} scale={scale} />}
       </div>
     </main>
@@ -62,9 +63,14 @@ function Sidebar({ recipes }: { recipes: Array<Recipe> }) {
           .filter((r) => r.label.toLowerCase().includes(search.toLowerCase()))
           .map((recipe) => (
             <li key={recipe.label}>
-              <Link
+              <NavLink
                 to={"/" + recipe.label}
-                className="rounded shadow block w-32 h-36 hover:shadow-slate-400 transition-all overflow-hidden whitespace-nowrap"
+                className={({ isActive }) =>
+                  cn(
+                    "rounded shadow block w-32 h-36 hover:shadow-slate-400 transition-all overflow-hidden whitespace-nowrap",
+                    isActive && "font-bold bg-red-400",
+                  )
+                }
               >
                 <p className="text-center w-11/12 mx-auto overflow-x-hidden text-ellipsis">
                   {recipe.label}
@@ -75,11 +81,53 @@ function Sidebar({ recipes }: { recipes: Array<Recipe> }) {
                     className="aspect-square hover:scale-110 transition-all"
                   />
                 </div>
-              </Link>
+              </NavLink>
             </li>
           ))}
       </ul>
     </aside>
+  );
+}
+
+function ScaleSelector({
+  scale,
+  setScale,
+}: {
+  scale: number;
+  setScale: Dispatch<SetStateAction<number>>;
+}) {
+  return (
+    <div className="w-fit mx-auto">
+      <div className="flex">
+        <button
+          className="text-lg p-2"
+          onClick={() => setScale((s) => Math.max(s - 1, MIN))}
+        >
+          {"<"}
+        </button>
+        <input
+          value={scale}
+          className="w-12 text-center font-medium text-lg block"
+          onChange={(e) => setScale(parseInt(e.currentTarget.value))}
+        />
+        <button
+          className="text-lg p-2"
+          onClick={() => setScale((s) => Math.min(s + 1, MAX))}
+        >
+          {">"}
+        </button>
+      </div>
+
+      <input
+        type="range"
+        min={MIN}
+        max={MAX}
+        step={1}
+        className="w-24"
+        value={scale}
+        onChange={(e) => setScale(parseInt(e.currentTarget.value))}
+      />
+    </div>
   );
 }
 
